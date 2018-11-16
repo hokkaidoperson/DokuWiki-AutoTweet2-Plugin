3ad5050df3950409fc9568e15d71d073451d1d9e
Nicolas Friedli <nicolas@theologique.ch>
2018-11-16 20:35:12 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['apiKey']                = 'Votre clé (API Key) ';
+$lang['apiSecret']             = 'Votre code (API Secret)';
+$lang['accessToken']           = 'Votre jeton d\'accès (Access Token)';
+$lang['accessTokenSecret']     = 'Votre code d\'accès (Acces Token Secret)';
+$lang['subjectOfTweet']        = 'Sujets des auto-tweets';
+$lang['subjectOfTweet_edit']   = 'édité';
+$lang['subjectOfTweet_create'] = 'créé';
+$lang['subjectOfTweet_delete'] = 'effacé';
+$lang['subjectOfTweet_minor']  = 'édité (modifications mineures)';
+$lang['guestIP_o_0']           = 'Ne pas montrer l\'adresse IP, remplacée par un champ vide';
+$lang['guestIP_o_alt']         = 'Remplacer par un texte alternatif (l\'extension Hidingip est nécessaire).';
+$lang['guestIP_o_show']        = 'Montrer l\'adresse IP';
