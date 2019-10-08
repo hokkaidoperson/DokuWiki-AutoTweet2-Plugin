25b8f4b3bc5e5fac0344531ba52ee23af7573207
Bartek S <sadupl@gmail.com>
2019-10-08 20:15:31 +0200

    translation update

---
+++ lang/pl/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['apiKey']                = 'Klucz API który posiadasz';
+$lang['subjectOfTweet_edit']   = 'edytowany';
+$lang['subjectOfTweet_revert'] = 'cofnięte';
+$lang['subjectOfTweet_create'] = 'stworzony';
+$lang['subjectOfTweet_delete'] = 'skasowany';
+$lang['subjectOfTweet_minor']  = 'edytowane (niewielka zmiana)';
+$lang['guestIP_o_0']           = 'Nie pokazuj adresu IP, zamień na pusty tekst';
+$lang['guestIP_o_alt']         = 'Zamień na alternatywny tekst (wymagana wtyczka "Hidingip".)';
+$lang['guestIP_o_show']        = 'Pokaż adres IP';
